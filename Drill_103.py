616742089c25c0ff4b5da5f7bcbe1840ed24e920
Rebelnote <55725601+Rebelnote@users.noreply.github.com>
2019-11-30 19:20:43 -0800

    Final pg_103 Drill

--- Drill_103.py
+++ Drill_103.py
@@ -1,17 +1,36 @@
+import sqlite3
 
-import os
+
+conn = sqlite3.connect('Drill_103.db')
 
 fileList = ('information.docx','Hello.txt','myImage.png', \
             'myMovie.mpg','World.txt','data.pdf','myPhoto.jpg')
 
+with conn:
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_files( \
+        ID  INTEGER PRIMARY KEY AUTOINCREMENT, \
+        col_fileNames TEXT \
+        )")
 
-def txtFiles():
-    for filename in fileList:
-        if filename.endswith('txt'):
-            print(os.path.join("fileList", filename))
-            continue
-        else:
-            continue
+for fileNames in fileList:
+    if fileNames.endswith(".txt"):
+        cur.execute("INSERT INTO tbl_files(col_FileNames) VALUES (?)", (fileNames,))
+    
+with conn:
+    cur = conn.cursor()
+    cur.execute("SELECT col_FileNames FROM tbl_files")
+    printFiles = cur.fetchall()
+    print(printFiles)
+    conn.commit()
+conn.close()
 
-if __name__ == "__main__": 
-    txtFiles()
+
+
+
+
+
+
+
+
+
